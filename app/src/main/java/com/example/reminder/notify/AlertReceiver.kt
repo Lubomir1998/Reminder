cd2e7bc695fbf4177b2d3b326bbd3dd70a9a4101
Lubomir1998 <lubcho_sk8@abv.bg>
2020-08-19 12:35:54 +0300

    Wake the screen after receiving a notification

--- app/src/main/java/com/example/reminder/notify/AlertReceiver.kt
+++ app/src/main/java/com/example/reminder/notify/AlertReceiver.kt
@@ -1,12 +1,10 @@
 package com.example.reminder.notify
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import com.example.reminder.db.Event
-import com.example.reminder.repository.Repository
-import javax.inject.Inject
+import android.os.PowerManager
 
 
 class AlertReceiver: BroadcastReceiver(){
@@ -14,19 +12,35 @@ class AlertReceiver: BroadcastReceiver(){
     private lateinit var notificationHelper: NotificationHelper
 
 
+    @SuppressLint("InvalidWakeLockTag")
     override fun onReceive(context: Context?, intent: Intent?) {
 
+        // wake the screen after receiving the notification
+        val pm = context!!.getSystemService(Context.POWER_SERVICE) as PowerManager
+        val isScreenOn = pm.isScreenOn
+        if (!isScreenOn) {
+            val wl = pm.newWakeLock(
+                PowerManager.FULL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP or PowerManager.ON_AFTER_RELEASE,
+                "MyLock"
+            )
+            wl.acquire(10000)
+            val wl_cpu = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyCpuLock")
+            wl_cpu.acquire(10000)
+        }
 
-        notificationHelper = context?.let {
-            NotificationHelper(it)
-        } ?: return
+
+        notificationHelper = NotificationHelper(context)
 
         val eventTitle = intent?.getStringExtra("title")
         val notificationId = intent?.getLongExtra("id", -1)
         val description = intent?.getStringExtra("description")
 
         // in createNotification(string) function we give the event title as a parameter
-        val notification = notificationHelper.createNotification(notificationId!!, eventTitle!!, description!!).build()
+        val notification = notificationHelper.createNotification(
+            notificationId!!,
+            eventTitle!!,
+            description!!
+        ).build()
 
         notificationHelper.getManager().notify(notificationId!!.toInt(), notification)
 
